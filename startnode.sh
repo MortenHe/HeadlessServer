7e1c2e10d3bf32e0540e031e4d418188cc45d92a
Martin Helfer <martin-helfer@gmx.de>
2018-09-06 14:51:58 +0200

    startnode

--- startnode.sh
+++ startnode.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd /home/pi/mh_prog/WebSocketHeadlessPlayerServer
 /usr/bin/sudo /usr/bin/node ./server.js > /home/pi/mh_prog/output-server.txt &
+
+/bin/sleep 5
+/usr/bin/sudo /usr/bin/node ./button.js > /home/pi/mh_prog/output-button.txt &
+/usr/bin/sudo /usr/bin/node ./rotary.js > /home/pi/mh_prog/output-rotary.txt &
